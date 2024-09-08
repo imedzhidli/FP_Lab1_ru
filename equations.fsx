0e581c70a133b54efee7056ae91f7e0ce733aad3
Ismail Medzhidli <i.medzhidov03@gmail.com>
2024-09-08 18:48:44 +0300

    FP_Lab1

--- equations.fsx
+++ equations.fsx
@@ -1,28 +1,49 @@
-// Определите функции для решение алгебраических уравнений
+//Задание 2: Численное решение трансцендентных уравнений
 
-let dichotomy f a b = 0.
+open System
+let eps = 0.0000000001
 
-let iterations phi x0 = 0.
+let rec dichotomy f (a: float) (b: float) (eps: float) = 
+    let c = (a + b) / 2.
+    if (abs (f c) >= eps) then
+        if (f a * f c < 0) then
+            dichotomy f a c eps
+        else
+            dichotomy f c b eps
+    else 
+        c
 
-let newthon f f' x0 = 0.
-// используйте функцию 'iterations'
+let rec iterations phi x0 eps = 
+    let c = phi x0
+    if (abs(x0 - c)) >= eps then
+        iterations phi c eps
+    else
+        x0
 
-// Решите 3 уравнения (начиная со своего номера варианта) с использованием 3-х методов
-let f1 = ...
-let f2 = ...
-let f3 = ...
+let newthon f f' x0 eps =
+    let phi x = x - (f x / f' x)
+    iterations phi x0 eps
 
-let f1' = ...
-let f2' = ...
-let f3' = ...
+let f1 (x: float) = Math.Exp(x) + Math.Sqrt(1. + Math.Exp(2. * x)) - 2.
+let f2 (x: float) = Math.Log(x) - x + 1.8
+let f3 (x: float) = x * Math.Tan(x) - 1./3.
 
-let phi1 = ...
-let phi2 = ...
-let phi3 = ...
+let f1' (x: float) = Math.Exp(x) + Math.Exp(x) / Math.Sqrt(1. + Math.Exp(2. * x))
+let f2' (x: float) = 1. / x - 1.
+let f3' (x: float) = Math.Tan(x) + x / Math.Cos(x)**2
 
+let phi1 (x: float) = x - f1 x / f1' x 
+let phi2 (x: float) = x - f2 x / f2' x 
+let phi3 (x: float) = x - f3 x / f3' x 
+
+
+printfn "----------------------------------------"
+printfn "| № Eq | Dichotomy |  Iters  | Newthon |"
+printfn "----------------------------------------"
 let main = 
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 1.) (iterations phi2 0.) (newthon f2 f2' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 1.)
+    printfn "|   1  |   %-8.4f| %7.4f | %7.4f |" (dichotomy f1 -1. 0. eps) (iterations phi1 -1. eps) (newthon f1 f1' -1. eps)
+    printfn "|   2  |   %-8.4f| %7.4f | %7.4f |" (dichotomy f2 2. 3. eps) (iterations phi2 2. eps) (newthon f2 f2' 2. eps)
+    printfn "|   3  |   %-8.4f| %7.4f | %7.4f |" (dichotomy f3 0.2 1. eps) (iterations phi3 0.2 eps) (newthon f3 f3' 0.2 eps)
 
- 
+printfn "----------------------------------------"
+main
--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,51 @@
-// Print a table of a given function f, computed by taylor series
+// Задание 1: Ряд Тейлора
 
-// function to compute
-let f = sin
+open System
+let buildinfunc x= (1. - float(Math.Pow(x, 2)/2.)) * float(cos(x)) - float(x)/2. * float(sin(x))
 
-let a = 0.0
-let b = 1.0
+let a = 0.1
+let b = 0.6
 let n = 10
+let eps = 0.0000000001
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+// Функция для вычисления факториала
+let fact n =
+    let rec fact' acc = function
+    |   n when n <= 1  -> acc 
+    |   n -> fact' (n*acc) (n-1)
+    fact' 1 n
+
+// Наивный Тейлор
+let taylor_naive x = 
+    let rec naive n acc term =
+        if abs(term) < eps then acc, n
+        else
+            let term = (-1.) ** n * (2. * n ** 2. + 1.) * (x ** (2. * n)) / float(fact(2 * int n)) //i-тый член
+            naive (n + 1.0) (acc + term) term
+    naive 1 1. 1.
 
 
-// Define a function to do the same in a more efficient way
-let taylor = f
+// Умный Тейлор
+let taylor_smart x = 
+    let rec taylor_0 n acc term = 
+        if abs(term) < eps then acc, n
+        else // Домножаем на высчитанную для этого ряда величину
+          let term = term * ((-1.) * x**2. * (2. * float n ** 2. + 1.) * float(fact(2 * int n - 2)) / (float(fact(2*int(n))) * (2. * (n - 1.)**2. + 1.))) 
+          taylor_0 (n + 1.0) (acc + term) term
+    taylor_0 1 1. 1.
+
+printfn "--------------------------------------------------------------------------------------"
+printfn "|   x   |  Builtin  |     Smart Taylor     | # terms |     Dumb Taylor     | # terms |"
+printfn "--------------------------------------------------------------------------------------"
 
 let main =
    for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
+    let x = a+(float i)/(float n)*(b-a)
+    let smart, terms1 = taylor_smart x
+    let naive, terms2 = taylor_naive x
+    printfn "| %5.3f |  %-9.6f|       %-14.6f |    %.0f    |       %-14.6f|    %.0f    |" x (buildinfunc x) smart terms1 naive terms2
+
+
+printfn "--------------------------------------------------------------------------------------"
 
 main
-
